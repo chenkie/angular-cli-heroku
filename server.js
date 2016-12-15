0312397176086b87ef8295c03bca32aa5f7a26cf
Ryan Chenkie <ryanchenkie@gmail.com>
2016-12-15 00:08:48 -0500

    fix routes

--- server.js
+++ server.js
@@ -26,9 +26,9 @@ app.use(express.static(__dirname + '/dist'));
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
-// app.get('/*', function(req, res) {
-//   res.sendFile(path.join(__dirname + '/dist/index.html'));
-// });
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 
 // Start the app by listening on the default
 // Heroku port

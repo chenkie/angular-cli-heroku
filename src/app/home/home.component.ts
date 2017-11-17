a5aae875699eac751c72aca4e78252d304e8569b
Chris Samuel <ksamuel.chris@gmail.com>
2017-11-17 12:44:00 -0500

    added the public vars

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,6 +9,15 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
+
+  public title = 'Chris Kayode ';
+  public socialFlare = ' Follow me on';
+  public socialNetwork1 = 'Twitter';
+  public socialNetwork2 = 'LinkedIn';
+  public socialNetwork3 = ' Github';
+  public emailAddress = 'email';
+  public AltSocialBlurb = 'or send me an';
+
   ngOnInit() {
   }
 
